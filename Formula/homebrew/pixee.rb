b32e5f7b1d2d24c013433f68a3486a174f1fcc2a
Daniel D'Avella <dan.davella@pixee.ai>
2023-12-19 21:27:31 -0500

    Fix previous commit: CLI update to 0.5.2

--- Formula/homebrew/pixee.rb
+++ Formula/homebrew/pixee.rb
@@ -6,8 +6,7 @@ class Pixee < Formula
   desc "Fix and prevent bugs and security vulnerabilities in your code"
   homepage "https://pixee.ai"
  
-  url "https://worker-brew.pixee.workers.dev/b8/1a/087990805162b5d347edf22f7c8a2b47c3e38f2db305068e6ae6161141bc/pixee-0.5.0.tar.gz"
-  url "https://worker-brew.pixee.workers.dev/79/e1/e7838e233f6b1ecd6447e8f34062cf96d063395e0d88e4c3f85dba28abc1/pixee-0.5.2.tar.gz
+  url "https://worker-brew.pixee.workers.dev/79/e1/e7838e233f6b1ecd6447e8f34062cf96d063395e0d88e4c3f85dba28abc1/pixee-0.5.2.tar.gz"
   sha256 "ea0583b04a719b09b68aedb18314a1d6e2ddd929292091b566b2bad1d4290718"
 
   #url "https://github.com/pixee/pixee-cli.git", tag: cli_tag
