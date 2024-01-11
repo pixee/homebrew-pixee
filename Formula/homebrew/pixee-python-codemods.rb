c4d8093341add9d74c738d05ee7c1d96c2b226ca
Daniel D'Avella <dan.davella@pixee.ai>
2024-01-11 09:32:26 -0500

    Update Python codemodder to 0.69.0

--- Formula/homebrew/pixee-python-codemods.rb
+++ Formula/homebrew/pixee-python-codemods.rb
@@ -2,8 +2,8 @@ class PixeePythonCodemods < Formula
     include Language::Python::Virtualenv
     desc "Python codemods for Pixee"
     homepage "https://github.com/pixee/codemodder-python"
-    url "https://files.pythonhosted.org/packages/ab/0a/c3d18b58b1e84a39ecc79ae295bfa7ff0ebd1a8d956578fb7cdb90d31b37/codemodder-0.66.0.tar.gz"
-    sha256 "029090dea9efcd7a044f579237e4483700e7c220330f22e7d729cb81be8c96b8"
+    url "https://files.pythonhosted.org/packages/6a/f7/cd6a38b98b9814a3ea9213036d69b9109e5007367cb049b6956ccb36b9e6/codemodder-0.69.0.tar.gz
+    sha256 "e092feddb65be523b8955d41a5adbeb584645dcddcdc07dc29b78fb8a8227f8f"
 
     depends_on "python@3.11"
 
