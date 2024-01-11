1df1c6347f69edda744cfaa90bef5bf49a1548ad
Daniel D'Avella <dan.davella@pixee.ai>
2024-01-11 11:55:18 -0500

    Fix unterminated string

--- Formula/homebrew/pixee-python-codemods.rb
+++ Formula/homebrew/pixee-python-codemods.rb
@@ -2,7 +2,7 @@ class PixeePythonCodemods < Formula
     include Language::Python::Virtualenv
     desc "Python codemods for Pixee"
     homepage "https://github.com/pixee/codemodder-python"
-    url "https://files.pythonhosted.org/packages/6a/f7/cd6a38b98b9814a3ea9213036d69b9109e5007367cb049b6956ccb36b9e6/codemodder-0.69.0.tar.gz
+    url "https://files.pythonhosted.org/packages/6a/f7/cd6a38b98b9814a3ea9213036d69b9109e5007367cb049b6956ccb36b9e6/codemodder-0.69.0.tar.gz"
     sha256 "e092feddb65be523b8955d41a5adbeb584645dcddcdc07dc29b78fb8a8227f8f"
 
     version "0.69.0"
